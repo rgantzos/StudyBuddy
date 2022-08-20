9cd8443c6c311cf4086201514681f2b239dacf5f
rgantzos <86856959+rgantzosonscratch@users.noreply.github.com>
2022-08-19 17:55:21 -0700

    Update background.js

--- background.js
+++ background.js
@@ -2,13 +2,11 @@ chrome.contextMenus.create({title: 'Add to Study Guide', id: 'studyBuddyBookmark
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId = 'studyBuddyBookmark') {
-    var domain = (new URL(tab.url));
-    domain = domain.protocol+'//'+domain.hostname;
     chrome.storage.sync.get("sites", function(obj) {
         if (obj.sites === undefined) {
-            chrome.storage.sync.set({"sites":[domain]})
+            chrome.storage.sync.set({"sites":[tab.url]})
         } else {
-            obj.sites.push(domain)
+            obj.sites.push(tab.url)
             chrome.storage.sync.set({"sites":obj.sites})
         }
     })
